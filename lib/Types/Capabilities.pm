0a45f6f54961fdca688d05340349f7f026c77d26
Toby Inkster <mail@tobyinkster.co.uk>
2025-11-14 14:02:45 +0000

    init

---
+++ lib/Types/Capabilities.pm
@@ -0,0 +1,50 @@
+use 5.010001;
+use strict;
+use warnings;
+
+package Types::Capabilities;
+
+our $AUTHORITY = 'cpan:TOBYINK';
+our $VERSION   = '0.001000';
+
+1;
+
+__END__
+
+=pod
+
+=encoding utf-8
+
+=head1 NAME
+
+Types::Capabilities - don't care what type of data you are given, just what you can do with it
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 BUGS
+
+Please report any bugs to
+L<https://github.com/tobyink/p5-types-capabilities/issues>.
+
+=head1 SEE ALSO
+
+=head1 AUTHOR
+
+Toby Inkster E<lt>tobyink@cpan.orgE<gt>.
+
+=head1 COPYRIGHT AND LICENCE
+
+This software is copyright (c) 2025 by Toby Inkster.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+
+=head1 DISCLAIMER OF WARRANTIES
+
+THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+
